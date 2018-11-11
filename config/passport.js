2d6a6348f19c04bc69b27c9e6a3e2866cae7413c
Jackster001 <wlin041@gmail.com>
2018-11-11 13:18:00 -0500

    updated

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,15 @@
-const JwtStrategy
+const JwtStrategy = require ('passport-jwt').strategy;
+const ExtractJwt= require('passport-jwt').ExtractJwt;
+const mongoose =requhire ('mongoose');
+const User= mongoose.model('users');
+const keys = require('../config/keys');
+
+const opts={};
+opts.jwtFormRequestion = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey=keys.secretOrKey;
+
+module.exports= passport =>{
+    passport.use(new JwtStrategy(opts,(jwt_payload, done)=>{
+        
+    }));
+}
